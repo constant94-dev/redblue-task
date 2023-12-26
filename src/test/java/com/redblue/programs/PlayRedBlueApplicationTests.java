42d8ae74df29e1d6ab1ff5e711d2093d19551215
Aaron(constant94-dev) <constant94@naver.com>
2023-12-27 04:59:45 +0900

    feat(programs): 프로젝트 초기 세팅

---
+++ src/test/java/com/redblue/programs/PlayRedBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redblue.programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayRedBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
