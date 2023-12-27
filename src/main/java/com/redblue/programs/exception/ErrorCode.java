5ffbe8fca30ee5e1502001afe569a07949c1530a
Aaron(constant94-dev) <constant94@naver.com>
2023-12-27 20:54:45 +0900

    feat(exception): 프로젝트 오류 처리 메서드 구현

---
+++ src/main/java/com/redblue/programs/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.redblue.programs.exception;
+
+import static org.springframework.http.HttpStatus.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    /* 403 Forbidden: 콘텐츠 접근할 권리 없음 */
+    // TODO: 401과 다른 점은 서버는 클라이언트가 누구인지 알고 있다
+    INVALID_BEARER_TOKEN(FORBIDDEN,"권한 정보가 없는 토큰입니다."),
+
+    /* 404 Not Found: 요청 리소스 찾을 수 없음 */
+    SMS_REQUEST_NOT_FOUND(NOT_FOUND,"요청 데이터를 찾을 수 없습니다."),
+
+    /* 500 Internal Server Error: 서버 처리 문제 */
+    SERVER_ERROR(INTERNAL_SERVER_ERROR,"서버 처리에 문제가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+}
---
+++ src/main/java/com/redblue/programs/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.redblue.programs.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/* @ControllerAdvice 프로젝트 전역에서 발생하는 모든 예외 잡기
+ * @ExceptionHandler 발생한 특정 예외 잡아 하나의 메서드로 공통 처리
+ * */
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(value = {CustomException.class})
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        LOGGER.info("handleCustomException throw CustomException: {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+}
