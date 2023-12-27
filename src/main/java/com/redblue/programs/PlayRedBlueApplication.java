4f141999a6a8861c4f93753c80f60fef12f6717e
Aaron(constant94-dev) <constant94@naver.com>
2023-12-27 20:52:48 +0900

    feat(resources): 서버 포트 변경, 스프링 응답 에러 설정

--- src/main/java/com/redblue/programs/PlayRedBlueApplication.java
+++ src/main/java/com/redblue/programs/PlayRedBlueApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlayRedBlueApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PlayRedBlueApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PlayRedBlueApplication.class, args);
+    }
 }
