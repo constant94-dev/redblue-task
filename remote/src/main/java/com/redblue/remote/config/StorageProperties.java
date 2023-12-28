446690a11c9bbd8bcaed03afd2abc5581391f108
Aaron(constant94-dev) <constant94@naver.com>
2023-12-29 08:12:53 +0900

    feat(config): 서버 로컬 스토리지 설정 구현

--- remote/src/main/java/com/redblue/remote/config/StorageProperties.java
+++ remote/src/main/java/com/redblue/remote/config/StorageProperties.java
@@ -5,9 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("storage")
 public class StorageProperties {
-    /**
-     * Folder location for storing files
-     */
     @Value("${app.upload.dir:${user.home}}")
     private String location;
 
