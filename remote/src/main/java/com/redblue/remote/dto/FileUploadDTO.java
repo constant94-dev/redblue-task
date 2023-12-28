3c4cc0b580e61577abd0ed64622657190c5aa17e
Aaron(constant94-dev) <constant94@naver.com>
2023-12-28 19:59:55 +0900

    feat(dto): 클라와 서버 데이터베이스 CRUD 작업 dto 생성

---
+++ remote/src/main/java/com/redblue/remote/dto/FileUploadDTO.java
@@ -0,0 +1,12 @@
+package com.redblue.remote.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FileUploadDTO {
+    private String fileLocation;
+}
